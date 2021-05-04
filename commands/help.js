53ed03963f9bea0e825c433b0ee50bbe81519c5f
brandon-vo <76707560+brandon-vo@users.noreply.github.com>
2021-05-04 18:24:29 -0400

    Update

--- commands/help.js
+++ commands/help.js
@@ -10,20 +10,12 @@ module.exports = {
             .setColor('#11806A')
             .setAuthor('Valorant Stats', 'https://bit.ly/32GAtTp')
             .addFields(
-<<<<<<< HEAD
-                { name: 'Career Stats', value: "`$stats`: Display career stats of a user" },
-                { name: 'Last 20 Matches', value: "`$last20` `$l20`: Display stats for the last 20 matches of a user" },
-                { name: 'Last Game', value: "`$lastmatch` `$lm`: Display last match information of a user" },
-                { name: 'Link Account', value: "`$link`: Link a Valorant account to your Discord ID"},
-                { name: 'Extra', value: "`$joke` `$ping` `$help` `$join` `$leave`" },
-=======
                 { name: 'Career Stats', value: "`v!stats`: Display career stats of a user" },
                 //{ name: 'Last 20 Matches', value: "`v!last20` `v!l20`: Display stats for the last 20 matches of a user" },
                 { name: 'Last Game', value: "`v!lastmatch` `v!lm`: Display last match information of a user" },
                 { name: 'Link Account (bug)', value: "`v!link`: Link a Valorant account to your Discord ID" },
                 { name: 'Invite', value: "`v!invite`: Get Discord bot invite link" },
                 { name: 'Extra', value: "`v!joke` `v!ping` `v!help` `v!join` `v!leave`" },
->>>>>>> 0fa1075 (Update Bot)
             )
             .setTimestamp()
 
