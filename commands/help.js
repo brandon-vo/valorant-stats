34ab8335666f98450d89c5ed499e3aaa9525e7aa
brandon-vo <76707560+brandon-vo@users.noreply.github.com>
2021-05-05 11:41:06 -0400

    Update help command

--- commands/help.js
+++ commands/help.js
@@ -7,15 +7,16 @@ module.exports = {
     execute(message) {
 
         const helpEmbed = new MessageEmbed()
-            .setColor('#11806A')
-            .setAuthor('Valorant Stats', 'https://bit.ly/32GAtTp')
+            .setColor('RANDOM')
+            .setAuthor('Bot Commands', 'https://bit.ly/32GAtTp')
             .addFields(
-                { name: 'Career Stats', value: "`v!stats`: Display competitive career stats of a user" },
-                //{ name: 'Last 20 Matches', value: "`v!last20` `v!l20`: Display stats for the last 20 matches of a user" },
-                { name: 'Last Game', value: "`v!lastmatch` `v!lm`: Display last match information of a user" },
-                { name: 'Link Account (bug)', value: "`v!link`: Link a Valorant account to your Discord ID" },
-                { name: 'Invite', value: "`v!invite`: Get Discord bot invite link" },
-                { name: 'Extra', value: "`v!joke` `v!ping` `v!help`" },
+                {name: 'How to use this Discord bot', value: "```fix\n" + "1. Sign in with your Riot ID on https://tracker.gg/valorant\n"
+                + "2. Track your Valorant stats with the following commands \n```"},
+                { name: 'Career Stats `v!stats <username#tag>`', value: "Display competitive career stats of a user" },
+                { name: 'Last Game `v!lm <username#tag>`', value: "Display last match information of a user" },
+                { name: 'Link Account `v!link <username#tag>` (bug)', value: "Link a Valorant account to your Discord ID" },
+                { name: 'Invite Link `v!invite`', value: "Get Discord bot invite link" },
+                { name: 'Extra `v!joke` `v!ping` `v!help`', value: "\u200B" },
             )
             .setTimestamp()
 
