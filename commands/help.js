eeee2f5b34ba3334e67641b13ed2e70b8de314ac
brandon-vo <76707560+brandon-vo@users.noreply.github.com>
2021-05-09 23:27:47 -0400

    Update help command

--- commands/help.js
+++ commands/help.js
@@ -1,17 +1,21 @@
 const { MessageEmbed } = require('discord.js')
+const pagination = require('discord.js-pagination')
 
 module.exports = {
     name: "help",
-    description: "Display avaliable commands to the user.",
+    description: "Display avaliable commands and other helpful information to the user.",
 
     execute(message) {
 
-        const helpEmbed = new MessageEmbed()
+        const helpEmbed1 = new MessageEmbed()
             .setColor('RANDOM')
-            .setAuthor('Bot Commands', 'https://bit.ly/32GAtTp')
+            .setAuthor('Help Menu', 'https://bit.ly/32GAtTp')
             .addFields(
-                {name: 'How to use this Discord bot', value: "```fix\n" + "1. Sign in with your Riot ID on https://tracker.gg/valorant\n"
-                + "2. Track your Valorant stats with the following commands \n```"},
+                {
+                    name: 'How to use this Discord bot', value: "```fix\n" + "1. Sign in with your Riot ID on https://tracker.gg/valorant\n"
+                        + "2. Track your Valorant stats with the following commands \n```"
+                },
+                { name: '\u200B', value: "```md\n" + "#                      Bot Commands                        #" + "\n```" },
                 { name: 'Competitive Stats `v!stats <username#tag>` `v!comp <username#tag>`', value: "Display competitive career stats of a user" },
                 { name: 'Unrated Stats `v!unrated <username#tag>`', value: "Display unrated career stats of a user" },
                 { name: 'Spike Rush Stats `v!spikerush <username#tag>`', value: "Display unrated career stats of a user" },
@@ -25,7 +29,31 @@ module.exports = {
             )
             .setTimestamp()
 
-        message.channel.send(helpEmbed);
+        const helpEmbed2 = new MessageEmbed()
+            .setColor('RANDOM')
+            .setAuthor('Help Menu', 'https://bit.ly/32GAtTp')
+            .addFields(
+                { name: '\u200B', value: "```md\n" + "#                         Other                           #" + "\n```" },
+                { name: 'Note', value: "The bot must have access to the default permissions in order to function properly"},
+                { name: 'KDR', value: "kills / deaths", inline: true },
+                { name: 'KDA', value: "(kills + assists) / deaths)", inline: true },
+                { name: 'KAD', value: "(kills / deaths) + assists", inline: true },
+                { name: 'Known Bugs', value: 'Link command resets\nPlayers who have not played all gamemodes may receive errors\nStats not being retrieved by the bot - Try again later' },
+                { name: 'Found Bugs? ', value: "Contact CMDRVo#3496 on Discord" },
+            )
+            .setTimestamp()
+
+        // Pages
+        const helpPages = [
+            helpEmbed1,
+            helpEmbed2
+        ]
+
+        const flipPage = ["⬅️", "➡️"]
+
+        const timeout = '100000'
+
+        pagination(message, helpPages, flipPage, timeout)
 
     }
 
