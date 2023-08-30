6bd592738516fb08d14ea92742d75cc59ad1f0cd
Brandon Vo <b4vo@uwaterloo.ca>
2023-08-30 01:06:20 -0400

    v3.2: Fix bug

--- functions/getArgs.js
+++ functions/getArgs.js
@@ -7,7 +7,7 @@ async function getArgs(interaction) {
 
   const account = await Account.find({ discordId: interaction.user.id });
 
-  if (!args) {
+  if (!args && account) {
     args = account[0].valorantAccount;
   }
 
