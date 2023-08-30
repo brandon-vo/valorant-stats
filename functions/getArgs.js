af7a50632936698f6f567923041e6e52d323783c
Brandon Vo <b4vo@uwaterloo.ca>
2023-08-30 01:10:15 -0400

    v3.2: Fix bug

--- functions/getArgs.js
+++ functions/getArgs.js
@@ -3,15 +3,12 @@ const { noAccountEmbed, errorEmbed } = require('../components/embeds');
 const { buttons, helpButtons } = require('../components/buttons');
 
 async function getArgs(interaction) {
-  let args = interaction.options.getString('username-tag');
 
   const account = await Account.find({ discordId: interaction.user.id });
 
-  if (!args) {
-    args = account[0]?.valorantAccount;
-  }
+  let args = interaction.options.getString('username-tag') || account[0]?.valorantAccount;
 
-  if (args.includes('@')) {
+  if (args?.includes('@')) {
     try {
       // mentionedID = args.split('!')[1].slice(0, -1); // old doesn't work
       mentionedID = args.replace(/\D/g, '');
